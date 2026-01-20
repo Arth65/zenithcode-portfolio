146d0521767c42ae73100775cd3d78add554729c
Arth <arth.pathak010@gmail.com>
2026-01-20 17:54:00 +0530

    Updated config file

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
       },
     ],
   },
